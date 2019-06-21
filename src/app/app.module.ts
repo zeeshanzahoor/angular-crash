0b681286b5ffd4d4ae7698f92e60fc969bb7ffd9
Zeeshan Zahoor <zahoorzeeshan1@gmail.com>
2019-06-21 20:08:53 +0300

    app init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  declarations: [
+    AppComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
